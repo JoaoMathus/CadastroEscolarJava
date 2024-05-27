ea056c0db3d853e1f8ae5d1b6c3afc9b04967fc0
JoaoMathus <joaomatheusGDO@gmail.com>
2024-05-27 11:34:41 -0300

    Estrutura migrada.

---
+++ src/main/java/br/com/escola/cadastro/cadastroescolarjava/acessobanco/AlunoDao.java
@@ -0,0 +1,4 @@
+package br.com.escola.cadastro.cadastroescolarjava.acessobanco;
+
+public class AlunoDao {
+}
---
+++ src/main/java/br/com/escola/cadastro/cadastroescolarjava/entidades/Disciplina.java
@@ -0,0 +1,4 @@
+package br.com.escola.cadastro.cadastroescolarjava.entidades;
+
+public class Disciplina {
+}
