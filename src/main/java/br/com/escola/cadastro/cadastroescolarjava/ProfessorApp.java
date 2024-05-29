5ff071a6c8fcad43358e0005aeee4f5422ba6422
JoaoMathus <joaomatheusGDO@gmail.com>
2024-05-28 21:32:57 -0300

    Nova funcionalidade em ProfessorController: agora é possível mudar de tela (para o cadastro de aluno).

--- src/main/java/br/com/escola/cadastro/cadastroescolarjava/ProfessorApp.java
+++ src/main/java/br/com/escola/cadastro/cadastroescolarjava/ProfessorApp.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class professorApp extends Application {
+public class ProfessorApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(professorApp.class.getResource("professorCad.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ProfessorApp.class.getResource("professorCad.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Cadastro Escolar");
         stage.setScene(scene);
