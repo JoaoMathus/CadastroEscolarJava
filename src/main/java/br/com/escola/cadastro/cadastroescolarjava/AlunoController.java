7c3006624e414ab8b16f88318241d73e7750425c
JoaoMathus <joaomatheusGDO@gmail.com>
2024-05-29 15:01:44 -0300

    Tela de cadastro de aluno: quase pronta. Falta apenas selecionar a turma. Logo, depende da tela de cadastro de turmas.

--- src/main/java/br/com/escola/cadastro/cadastroescolarjava/AlunoController.java
+++ src/main/java/br/com/escola/cadastro/cadastroescolarjava/AlunoController.java
@@ -1,8 +1,12 @@
 package br.com.escola.cadastro.cadastroescolarjava;
 
+import br.com.escola.cadastro.cadastroescolarjava.entidades.Aluno;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 
@@ -17,6 +21,20 @@ public class AlunoController {
     private MenuItem menuTurma;
     @FXML
     private MenuItem menuNota;
+    @FXML
+    private TextField txtMatricula;
+    @FXML
+    private TextField txtNome;
+    @FXML
+    private TextField txtTelefone;
+    @FXML
+    private TextField txtCelular;
+    @FXML
+    private TextField txtCpfResponsavel;
+    @FXML
+    private DatePicker dataNascimento;
+    @FXML
+    private ChoiceBox<String> escolhaTipoSanguineo;
 
     private AluApp application;
 
@@ -45,5 +63,14 @@ public class AlunoController {
         professorController.setApplication(application);
 
         application.getPrimaryStage().getScene().setRoot(root);
+    }
+
+    @FXML
+    protected void salvarAluno() {
+        Aluno a = new Aluno(1, txtNome.getText(), dataNascimento.getValue().toString(), txtMatricula.getText(),
+                txtTelefone.getText(), txtCelular.getText(), txtCpfResponsavel.getText(), escolhaTipoSanguineo.getValue().toString(),
+                "3", false, 1);
+
+        System.out.println(a.toString());
     }
 }
