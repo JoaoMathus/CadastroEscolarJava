50afca85eb9c4e9f26caa9621ac73a4dea02fb41
JoaoMathus <joaomatheusGDO@gmail.com>
2024-05-29 16:19:26 -0300

    Confirmação do cadastro adicionada.

--- src/main/java/br/com/escola/cadastro/cadastroescolarjava/AlunoController.java
+++ src/main/java/br/com/escola/cadastro/cadastroescolarjava/AlunoController.java
@@ -1,13 +1,18 @@
 package br.com.escola.cadastro.cadastroescolarjava;
 
-import br.com.escola.cadastro.cadastroescolarjava.entidades.Aluno;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class AlunoController {
     @FXML
@@ -76,11 +81,22 @@ public class AlunoController {
             alert.showAndWait();
             return;
         }
-        // TODO: Adicionar lógica para resgatar uma turma pelo número (que é o control da tela de cadastro).
-        // TODO: e então resgatar a "série" pelo número e adicioná-lo ao objeto do aluno.
-        application.getAlunoDao().inserir(txtNome.getText(), dataNascimento.getValue().toString(),
-                txtMatricula.getText(), txtTelefone.getText(), txtCelular.getText(), txtCpfResponsavel.getText(),
-                escolhaTipoSanguineo.getValue(), "Segunda", 1);
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmação");
+        alert.setHeaderText("Confirma esse cadastro?");
+        alert.setContentText("Um novo aluno será salvo no banco de dados");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // TODO: Adicionar lógica para resgatar uma turma pelo número (que é o control da tela de cadastro).
+            // TODO: e então resgatar a "série" pelo número e adicioná-lo ao objeto do aluno.
+            application.getAlunoDao().inserir(txtNome.getText(), dataNascimento.getValue().toString(),
+                    txtMatricula.getText(), txtTelefone.getText(), txtCelular.getText(), txtCpfResponsavel.getText(),
+                    escolhaTipoSanguineo.getValue(), "Segunda", 1);
+        } else {
+            return;
+        }
 
         application.getAlunoDao().selecionarTodos().forEach(System.out::println);
     }
