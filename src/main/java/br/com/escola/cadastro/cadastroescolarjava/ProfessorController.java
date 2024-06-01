78cf07487f3fbbc41b8d285d5b109285e320329a
Desisto17 <breno_serra10@hotmail.com>
2024-06-01 18:33:12 -0300

    Feito função salvarProfessor.

--- src/main/java/br/com/escola/cadastro/cadastroescolarjava/ProfessorController.java
+++ src/main/java/br/com/escola/cadastro/cadastroescolarjava/ProfessorController.java
@@ -1,11 +1,18 @@
 package br.com.escola.cadastro.cadastroescolarjava;
 
+import br.com.escola.cadastro.cadastroescolarjava.entidades.Aluno;
+import br.com.escola.cadastro.cadastroescolarjava.entidades.Professor;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 public class ProfessorController {
     @FXML
@@ -16,6 +23,27 @@ public class ProfessorController {
     private MenuItem menuTurma;
     @FXML
     private MenuItem menuNota;
+    @FXML
+    private TextField txtNome;
+    @FXML
+    private TextField txtTelefone;
+    @FXML
+    private TextField txtCelular;
+    @FXML
+    private TextField txtCpf;
+    @FXML
+    private DatePicker dataNascimento;
+    @FXML
+    private TextField txtPesquisa;
+    @FXML
+    private TableView<Professor> tabelaProfessores;
+    @FXML
+    private TableColumn<Professor, String> colunaCpf;
+    @FXML
+    private TableColumn<Professor, String> colunaNome;
+    @FXML
+    private TableColumn<Professor, String> colunaTurma;
+
 
     private AluApp application;
 
@@ -44,4 +72,36 @@ public class ProfessorController {
 
         application.getPrimaryStage().getScene().setRoot(root);
     }
+
+    @FXML
+    protected void salvarProfessor() {
+        if (txtNome.getText().isEmpty() || dataNascimento.getValue().toString().isEmpty() ||
+                txtTelefone.getText().isEmpty() || txtCelular.getText().isEmpty() ||
+                txtCpf.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erro ao salvar professor");
+            alert.setHeaderText("Não foi possível salvar o professor");
+            alert.setContentText("Certifique-se de preencher todos os campos!");
+
+            alert.showAndWait();
+            return;
+        }
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmação");
+        alert.setHeaderText("Confirma esse cadastro?");
+        alert.setContentText("Um novo professor será salvo no banco de dados");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // TODO: Adicionar lógica para resgatar uma turma pelo número (que é o control da tela de cadastro).
+            // TODO: e então resgatar a "série" pelo número e adicioná-lo ao objeto do aluno.
+            application.getProfessorDao().inserir(txtNome.getText(), dataNascimento.getValue().toString(),
+                    txtTelefone.getText(), txtCelular.getText(),
+                    txtCpf.getText());
+        }
+
+        application.getProfessorDao().selecionarTodos().forEach(System.out::println);
+    }
+
 }
