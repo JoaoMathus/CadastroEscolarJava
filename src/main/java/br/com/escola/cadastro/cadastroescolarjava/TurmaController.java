80a36683f7a9447707bf94e8e95dffdd8025d175
JoaoMathus <joaomatheusgdo@gmail.com>
2024-06-03 08:43:30 -0300

    Tela de turma atualizada.

--- src/main/java/br/com/escola/cadastro/cadastroescolarjava/TurmaController.java
+++ src/main/java/br/com/escola/cadastro/cadastroescolarjava/TurmaController.java
@@ -1,4 +1,70 @@
 package br.com.escola.cadastro.cadastroescolarjava;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
 public class TurmaController {
+
+    private AluApp application;
+    public void setApplication(AluApp application) {
+        this.application = application;
+    }
+
+    @FXML
+    protected void irParaAluno() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("alunoCad.fxml"));
+        BorderPane root = fxmlLoader.load();
+
+        AlunoController alunoController = fxmlLoader.getController();
+        alunoController.setApplication(application);
+
+        application.getPrimaryStage().getScene().setRoot(root);
+    }
+
+    @FXML
+    protected void irParaProfessor() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("professorCad.fxml"));
+        BorderPane root = fxmlLoader.load();
+
+        ProfessorController professorController = fxmlLoader.getController();
+        professorController.setApplication(application);
+
+        application.getPrimaryStage().getScene().setRoot(root);
+    }
+
+    @FXML
+    protected void irParaTurma() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("turmaCad.fxml"));
+        BorderPane root = fxmlLoader.load();
+
+        TurmaController turmaController = fxmlLoader.getController();
+        turmaController.setApplication(application);
+
+        application.getPrimaryStage().getScene().setRoot(root);
+    }
+
+    @FXML
+    protected void irParaNotas() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("notaCad.fxml"));
+        BorderPane root = fxmlLoader.load();
+
+        NotaController notaController = fxmlLoader.getController();
+        notaController.setApplication(application);
+
+        application.getPrimaryStage().getScene().setRoot(root);
+    }
+
+    @FXML
+    protected void irParaHistorico() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("historico.fxml"));
+        BorderPane root = fxmlLoader.load();
+
+        HistoricoController historicoController = fxmlLoader.getController();
+        historicoController.setApplication(application);
+
+        application.getPrimaryStage().getScene().setRoot(root);
+    }
 }
