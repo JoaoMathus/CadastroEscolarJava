a3f5e09ffbe55c8bbf9217fce93199cf990ac9ba
JoaoMathus <joaomatheusGDO@gmail.com>
2024-06-06 16:16:23 -0300

    Entidade Disciplina atualizada.

--- src/main/java/br/com/escola/cadastro/cadastroescolarjava/acessobanco/DisciplinaDao.java
+++ src/main/java/br/com/escola/cadastro/cadastroescolarjava/acessobanco/DisciplinaDao.java
@@ -8,26 +8,20 @@ import java.util.List;
 
 public class DisciplinaDao extends AbstratoDao <Disciplina, Integer> {
     private final String scriptTabela = "CREATE TABLE IF NOT EXISTS disciplina (\n" +
-            "    iddisciplina INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+            "    idDisciplina INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
             "    nome TEXT,\n" +
-            "    notabimestre1 REAL,\n" +
-            "    notabimestre2 REAL,\n" +
-            "    notabimestre3 REAL,\n" +
-            "    notabimestre4 REAL,\n" +
-            "    anoletivo TEXT,\n" +
+            "    anoLetivo TEXT,\n" +
             "    aprovado INTEGER,\n" +
-            "    fk_idaluno INTEGER,\n" +
-            "    FOREIGN KEY(fk_idaluno) REFERENCES aluno(idaluno)\n" +
+            "    fk_idAluno INTEGER,\n" +
+            "    FOREIGN KEY(fk_idAluno) REFERENCES aluno(idAluno)\n" +
             ")";
     private final String insertSql = "INSERT INTO disciplina (" +
-            "nome, notabimestre1, notabimestre2, notabimestre3, notabimestre4, anoletivo, " +
-            "aprovado, fk_idaluno) VALUES (" +
-            "?, ?, ?, ?, ?, ?, ?, ?)";
-    private final String deleteSql = "DELETE FROM disciplina WHERE iddisciplina = ?";
+            "nome, anoLetivo, aprovado, fk_idAluno) VALUES (" +
+            "?, ?, ?, ?)";
+    private final String deleteSql = "DELETE FROM disciplina WHERE idDisciplina = ?";
     private final String updateSql = "UPDATE disciplina SET nome = ?," +
-            "notabimestre1 = ?, notabimestre2 = ?, notabimestre3 = ?, notabimestre4 = ?, " +
-            "anoletivo = ?, aprovado = ?, fk_idaluno = ? WHERE iddisciplina = ?)";
-    private final String selectSql = "SELECT * FROM disciplina WHERE iddisciplina = ?";
+            "anoLetivo = ?, aprovado = ?, fk_idAluno = ? WHERE idDisciplina = ?)";
+    private final String selectSql = "SELECT * FROM disciplina WHERE idDisciplina = ?";
     private final String selectAllSql = "SELECT * FROM disciplina";
 
     public DisciplinaDao() {
@@ -43,24 +37,19 @@ public class DisciplinaDao extends AbstratoDao <Disciplina, Integer> {
         }
     }
 
-    public void inserir(String nome, float notabimestre1, float notabimestre2,
-                        float notabimestre3, float notabimestre4,
-                        String anoLetivo, boolean aprovado, int idAluno) {
+    @Override
+    public void inserir(Disciplina d) {
         // Ver se essa disciplina já existe
         var disciplinas = selecionarTodos();
         for (var disciplina : disciplinas) {
-            if (disciplina.getNome().equals(nome) && disciplina.getIdAluno() == idAluno)
+            if (disciplina.getNome().equals(d.getNome()) && disciplina.getIdAluno() == d.getIdAluno())
                 return;
         }
         try (var stmt = conectar().prepareStatement(insertSql)) {
-            stmt.setString(1, nome);
-            stmt.setFloat(2, notabimestre1);
-            stmt.setFloat(3, notabimestre2);
-            stmt.setFloat(4, notabimestre3);
-            stmt.setFloat(5, notabimestre4);
-            stmt.setString(6, anoLetivo);
-            stmt.setBoolean(7, aprovado);
-            stmt.setInt(8, idAluno);
+            stmt.setString(1, d.getNome());
+            stmt.setString(2, d.getAnoLetivo());
+            stmt.setBoolean(3, d.isAprovado());
+            stmt.setInt(4, d.getId());
 
             stmt.executeUpdate();
         } catch (SQLException ex) {
@@ -82,14 +71,10 @@ public class DisciplinaDao extends AbstratoDao <Disciplina, Integer> {
     public void alterar(Disciplina d) {
         try (var stmt = conectar().prepareStatement(updateSql)) {
             stmt.setString(1, d.getNome());
-            stmt.setFloat(2, d.getNotaBimestre1());
-            stmt.setFloat(3, d.getNotaBimestre2());
-            stmt.setFloat(4, d.getNotaBimestre3());
-            stmt.setFloat(5, d.getNotaBimestre4());
-            stmt.setString(6, d.getAnoLetivo());
-            stmt.setBoolean(7, d.isAprovado());
-            stmt.setInt(8, d.getIdAluno());
-            stmt.setInt(9, d.getId());
+            stmt.setString(2, d.getAnoLetivo());
+            stmt.setBoolean(3, d.isAprovado());
+            stmt.setInt(4, d.getIdAluno());
+            stmt.setInt(5, d.getId());
 
             stmt.executeUpdate();
         } catch (SQLException ex) {
@@ -105,11 +90,9 @@ public class DisciplinaDao extends AbstratoDao <Disciplina, Integer> {
             stmt.setInt(1, id);
             var r = stmt.executeQuery();
             while (r.next()) {
-                d = new Disciplina(r.getInt("iddisciplina"), r.getString("nome"),
-                        r.getFloat("notabimestre1"), r.getFloat("notabimestre2"),
-                        r.getFloat("notabimestre3"), r.getFloat("notabimestre4"),
-                        r.getString("anoletivo"),
-                        r.getBoolean("aprovado"), r.getInt("fk_idaluno"));
+                d = new Disciplina(r.getInt("idDisciplina"), r.getString("nome"),
+                        r.getString("anoLetivo"), r.getBoolean("aprovado"),
+                        r.getInt("fk_idAluno"));
             }
         } catch (SQLException ex) {
             System.err.println("Erro ao selecionar disciplina: " + ex.getMessage());
@@ -124,12 +107,9 @@ public class DisciplinaDao extends AbstratoDao <Disciplina, Integer> {
         try (var stmt = conectar().createStatement()) {
             var r = stmt.executeQuery(selectAllSql);
             while (r.next()) {
-                lista.add(new Disciplina(r.getInt("iddisciplina"),
-                        r.getString("nome"), r.getFloat("notabimestre1"),
-                        r.getFloat("notabimestre2"), r.getFloat("notabimestre3"),
-                        r.getFloat("notabimestre4"),
-                        r.getString("anoletivo"), r.getBoolean("aprovado"),
-                        r.getInt("fk_idaluno")));
+                lista.add(new Disciplina(r.getInt("idDisciplina"),
+                        r.getString("nome"), r.getString("anoLetivo"),
+                        r.getBoolean("aprovado"), r.getInt("fk_idAluno")));
             }
         } catch (SQLException ex) {
             System.err.println("Erro ao selecionar todas as disciplinas: " + ex.getMessage());
--- src/main/java/br/com/escola/cadastro/cadastroescolarjava/entidades/Disciplina.java
+++ src/main/java/br/com/escola/cadastro/cadastroescolarjava/entidades/Disciplina.java
@@ -3,23 +3,14 @@ package br.com.escola.cadastro.cadastroescolarjava.entidades;
 public class Disciplina {
     private int id;
     private String nome;
-    private float notaBimestre1;
-    private float notaBimestre2;
-    private float notaBimestre3;
-    private float notaBimestre4;
     private String anoLetivo;
     private boolean aprovado;
     private int idAluno;
 
-    public Disciplina(int id, String nome, float notaBimestre1, float notaBimestre2,
-                      float notaBimestre3, float notaBimestre4, String anoLetivo,
+    public Disciplina(int id, String nome, String anoLetivo,
                       boolean aprovado, int idAluno) {
         this.id = id;
         this.nome = nome;
-        this.notaBimestre1 = notaBimestre1;
-        this.notaBimestre2 = notaBimestre2;
-        this.notaBimestre3 = notaBimestre3;
-        this.notaBimestre4 = notaBimestre4;
         this.anoLetivo = anoLetivo;
         this.aprovado = aprovado;
         this.idAluno = idAluno;
@@ -63,38 +54,6 @@ public class Disciplina {
         this.aprovado = aprovado;
     }
 
-    public float getNotaBimestre1() {
-        return notaBimestre1;
-    }
-
-    public void setNotaBimestre1(float notaBimestre1) {
-        this.notaBimestre1 = notaBimestre1;
-    }
-
-    public float getNotaBimestre2() {
-        return notaBimestre2;
-    }
-
-    public void setNotaBimestre2(float notaBimestre2) {
-        this.notaBimestre2 = notaBimestre2;
-    }
-
-    public float getNotaBimestre3() {
-        return notaBimestre3;
-    }
-
-    public void setNotaBimestre3(float notaBimestre3) {
-        this.notaBimestre3 = notaBimestre3;
-    }
-
-    public float getNotaBimestre4() {
-        return notaBimestre4;
-    }
-
-    public void setNotaBimestre4(float notaBimestre4) {
-        this.notaBimestre4 = notaBimestre4;
-    }
-
     public float calcularMedia(float nota1, float nota2,
                                float nota3, float nota4) {
         return (nota1 + nota2 + nota3 + nota4) / 4;
@@ -105,10 +64,6 @@ public class Disciplina {
         return "Disciplina{" +
                 "id=" + id +
                 ", nome='" + nome + '\'' +
-                ", notaBimestre1=" + notaBimestre1 +
-                ", notaBimestre2=" + notaBimestre2 +
-                ", notaBimestre3=" + notaBimestre3 +
-                ", notaBimestre4=" + notaBimestre4 +
                 ", anoLetivo='" + anoLetivo + '\'' +
                 ", aprovado=" + aprovado +
                 ", idAluno=" + idAluno +
